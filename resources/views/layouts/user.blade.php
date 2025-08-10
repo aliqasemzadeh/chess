f4cc149d338abb2a030618045429fd5d9f354248
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-10 17:31:56 +0330

    Remove BladewindUI dependencies from admin and user layouts.

--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -4,9 +4,6 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
-    <link href="{{ asset('vendor/bladewind/css/animate.min.css') }}" rel="stylesheet" />
-    <link href="{{ asset('vendor/bladewind/css/bladewind-ui.min.css') }}" rel="stylesheet" />
-    <script src="{{ asset('vendor/bladewind/js/helpers.js') }}"></script>
     @livewireStyles
     <title>{{ $title ?? 'User' }}</title>
 </head>
