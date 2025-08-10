186716cf74e2129310ef02dfee269082faf23c72
Ali Qasemzadeh <aliqasemzadeh7@gmail.com>
2025-08-10 15:36:24 +0330

    Add `#[Layout]` attributes to Livewire components for consistent layouts across user roles

--- app/Livewire/User/Game/Play.php
+++ app/Livewire/User/Game/Play.php
@@ -2,10 +2,12 @@
 
 namespace App\Livewire\User\Game;
 
+use Livewire\Attributes\Layout;
 use Livewire\Component;
 
 class Play extends Component
 {
+    #[Layout('layouts.user')]
     public function render()
     {
         return view('livewire.user.game.play');
